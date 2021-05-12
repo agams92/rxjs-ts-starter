b5a6ce9aec2e367c52ef8f4a361165409e2247e4
Lev <agams92@gmail.com>
2021-05-12 07:33:31 +0300

    не очень понятно, но робит

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
-import { of, from } from 'rxjs';
+import { empty, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { map, catchError, filter } from "rxjs/operators";
 
-const o = of(5) // Promise.resolve(5)
+const REPO_QUERY = "ndse_";
+const GITHUB_REPO_API = "https://api.github.com/search/repositories?q=";
 
-o.subscribe({
-  next: (value: any) => console.log('Next:', value),
-  complete: () => console.log('Complete!'),
-  error: (error) => console.log('Error!', error)
-})
+const JSON_PLACEHOLDER_API = "https://jsonplaceholder.typicode.com/posts";
+
+const getForks = (forksUrl) =>
+  ajax
+    .getJSON(forksUrl)
+    .pipe(map((response) => response))
+    .subscribe((forks) => console.log(forks));
+
+const observerGithub = ajax.getJSON(`${GITHUB_REPO_API}${REPO_QUERY}`).pipe(
+  map((response: any) => response.items),
+  map((repos) => {
+    console.log(repos);
+    return repos.map((repo) => getForks(repo.forks_url));
+  }),
+  catchError((error) => {
+    console.log("error: ", error);
+    return of(error);
+  })
+);
+
+observerGithub.subscribe({
+  next: (value: any) => console.log("Next:", value),
+  complete: () => console.log("Complete!"),
+  error: (error) => console.log("Error!", error),
+});
+
+const observerJSON = ajax.getJSON(JSON_PLACEHOLDER_API).pipe(
+  map((posts) => posts),
+  filter((post: any) => post.id > 30)
+);
+
+observerJSON.subscribe((post) => {
+  console.log(post);
+});
